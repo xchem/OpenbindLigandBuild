2e14a703d1b18070c25b867871f3885b19183b65
ConorFWild <conor.wild@sky.com>
2025-10-14 17:04:28 +0100

    First

--- test.py
+++ test.py
@@ -119,7 +119,7 @@ def remove_nearby_atoms(pdb_file, coord, radius, output_file):
                 
                 if add_res:
                     print(new_st[model.num])
-                    new_st[model.num][chain.name].add_residue(res)
+                    new_st[int(model.num)][chain.name].add_residue(res)
     
     new_st.write_pdb(str(output_file))
 
